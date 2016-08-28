1209b9ad712f6b2a682e46d2fa430620a4ce0433
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2016-08-28 15:42:57 -0500

    add babel-polyfill in browser test page

--- bin/client.js
+++ bin/client.js
@@ -1,136 +1,104 @@
-const https = require('https');
+/* eslint no-console: 'off', max-len: 'off' */
+const https = require('https')
 
 const data = {
   framework: 'mocha',
   url: 'http://localhost:9000/build/test/test.html',
   platforms: [{
-      browserName: 'internet explorer',
-      platform: 'Windows 7',
-      version: '11.0',
-      recordVideo: false,
-    },
-
-    {
-      browserName: 'chrome',
-      platform: 'Windows 7',
-      version: '52.0',
-      recordVideo: false,
-    },
-
-    {
-      browserName: 'chrome',
-      platform: 'Windows 7',
-      version: '51.0',
-      recordVideo: false,
-    },
-
-    {
-      browserName: 'chrome',
-      platform: 'Windows 7',
-      version: '50.0',
-      recordVideo: false,
-    },
-
-    {
-      browserName: 'firefox',
-      platform: 'Windows 7',
-      version: '46.0',
-      recordVideo: false,
-    },
-
-    {
-      browserName: 'firefox',
-      platform: 'Windows 7',
-      version: '47.0',
-      recordVideo: false,
-    },
-
-    {
-      browserName: 'firefox',
-      platform: 'Windows 7',
-      version: '48.0',
-      recordVideo: false,
-    },
-
-    {
-      browserName: 'MicrosoftEdge',
-      platform: 'Windows 10',
-      version: '13.10586',
-      recordVideo: false,
-    },
-
-    {
-      browserName: 'firefox',
-      platform: 'Windows 10',
-      version: '46.0',
-      recordVideo: false,
-    },
-
-    {
-      browserName: 'internet explorer',
-      platform: 'Windows 10',
-      version: '11.103',
-      recordVideo: false,
-    },
-
-    {
-      browserName: 'internet explorer',
-      platform: 'Windows 8.1',
-      version: '11.0',
-      recordVideo: false,
-    },
-
-    {
-      browserName: 'safari',
-      platform: 'OS X 10.9',
-      version: '7.0',
-      recordVideo: false,
-    },
-
-    {
-      browserName: 'safari',
-      platform: 'OS X 10.10',
-      version: '8.0',
-      recordVideo: false,
-    },
-
-    {
-      browserName: 'safari',
-      platform: 'OS X 10.11',
-      version: '9.0',
-      recordVideo: false,
-    },
-
-    {
-      browserName: 'Safari',
-      appiumVersion: '1.5.3',
-      recordVideo: false,
-      deviceName: 'iPhone 6s Simulator',
-      deviceOrientation: 'portrait',
-      platformVersion: '9.3',
-      platformName: 'iOS',
-    },
-
-    {
-      browserName: 'Safari',
-      appiumVersion: '1.5.3',
-      recordVideo: false,
-      deviceName: 'iPhone 6 Simulator',
-      deviceOrientation: 'portrait',
-      platformVersion: '9.2',
-      platformName: 'iOS',
-    },
-
-    {
-      browserName: 'Safari',
-      appiumVersion: '1.5.3',
-      recordVideo: false,
-      deviceName: 'iPhone 5s Simulator',
-      deviceOrientation: 'portrait',
-      platformVersion: '9.1',
-      platformName: 'iOS',
-    },
-  ],
+    browserName: 'internet explorer',
+    platform: 'Windows 7',
+    version: '11.0',
+    recordVideo: false,
+  }, {
+    browserName: 'chrome',
+    platform: 'Windows 7',
+    version: '52.0',
+    recordVideo: false,
+  }, {
+    browserName: 'chrome',
+    platform: 'Windows 7',
+    version: '51.0',
+    recordVideo: false,
+  }, {
+    browserName: 'chrome',
+    platform: 'Windows 7',
+    version: '50.0',
+    recordVideo: false,
+  }, {
+    browserName: 'firefox',
+    platform: 'Windows 7',
+    version: '46.0',
+    recordVideo: false,
+  }, {
+    browserName: 'firefox',
+    platform: 'Windows 7',
+    version: '47.0',
+    recordVideo: false,
+  }, {
+    browserName: 'firefox',
+    platform: 'Windows 7',
+    version: '48.0',
+    recordVideo: false,
+  }, {
+    browserName: 'MicrosoftEdge',
+    platform: 'Windows 10',
+    version: '13.10586',
+    recordVideo: false,
+  }, {
+    browserName: 'firefox',
+    platform: 'Windows 10',
+    version: '46.0',
+    recordVideo: false,
+  }, {
+    browserName: 'internet explorer',
+    platform: 'Windows 10',
+    version: '11.103',
+    recordVideo: false,
+  }, {
+    browserName: 'internet explorer',
+    platform: 'Windows 8.1',
+    version: '11.0',
+    recordVideo: false,
+  }, {
+    browserName: 'safari',
+    platform: 'OS X 10.9',
+    version: '7.0',
+    recordVideo: false,
+  }, {
+    browserName: 'safari',
+    platform: 'OS X 10.10',
+    version: '8.0',
+    recordVideo: false,
+  }, {
+    browserName: 'safari',
+    platform: 'OS X 10.11',
+    version: '9.0',
+    recordVideo: false,
+  }, {
+    browserName: 'Safari',
+    appiumVersion: '1.5.3',
+    recordVideo: false,
+    deviceName: 'iPhone 6s Simulator',
+    deviceOrientation: 'portrait',
+    platformVersion: '9.3',
+    platformName: 'iOS',
+  }, {
+    browserName: 'Safari',
+    appiumVersion: '1.5.3',
+    recordVideo: false,
+    deviceName: 'iPhone 6 Simulator',
+    deviceOrientation: 'portrait',
+    platformVersion: '9.2',
+    platformName: 'iOS',
+  }, {
+    browserName: 'Safari',
+    appiumVersion: '1.5.3',
+    recordVideo: false,
+    deviceName: 'iPhone 5s Simulator',
+    deviceOrientation: 'portrait',
+    platformVersion: '9.1',
+    platformName: 'iOS',
+  }],
 }
 
 data.platforms = data.platforms.filter(p => !('appiumVersion' in p)).map(p => [p.platform, p.browserName, p.version])
@@ -142,27 +110,27 @@ const options = {
   hostname: 'saucelabs.com',
   auth: 'longztian:ecd15b59-b05f-4a82-a54f-97e97b859965',
   headers: {
-    'Accept': 'application/json',
+    Accept: 'application/json',
     'Content-Type': 'application/json',
-    'Content-Length': body != null ? Buffer.byteLength(body) : 0
+    'Content-Length': body != null ? Buffer.byteLength(body) : 0,
   },
-};
+}
 
-var req = https.request(options, (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
+const req = https.request(options, (res) => {
+  console.log('statusCode:', res.statusCode)
+  console.log('headers:', res.headers)
 
   res.on('data', (d) => {
-    process.stdout.write(d);
-  });
-});
+    process.stdout.write(d)
+  })
+})
 
 // post the data
 if (body != null) {
-  req.write(body);
+  req.write(body)
 }
-req.end();
+req.end()
 
 req.on('error', (e) => {
-  console.error(e);
-});
+  console.error(e)
+})
