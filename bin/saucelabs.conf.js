293bc2cc6dc2d7780916ac25078640db3198b5e6
Long <15257546+lzztt@users.noreply.github.com>
2016-08-28 21:58:38 -0500

    specify tunnel id for sauce labs

--- bin/saucelabs.conf.js
+++ bin/saucelabs.conf.js
@@ -1,82 +1,69 @@
 module.exports = {
   name: `markx #${process.env.BUILD.slice(0, 7)} @ node ${process.env.NODE}`,
   build: process.env.BUILD,
+  tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
+  "recordVideo": false,
   framework: 'mocha',
   url: 'http://localhost:9000/build/test/test.html',
   platforms: [{
     browserName: 'internet explorer',
     platform: 'Windows 7',
     version: '11.0',
-    recordVideo: false,
   }, {
     browserName: 'chrome',
     platform: 'Windows 7',
     version: '52.0',
-    recordVideo: false,
   }, {
     browserName: 'chrome',
     platform: 'Windows 7',
     version: '51.0',
-    recordVideo: false,
   }, {
     browserName: 'chrome',
     platform: 'Windows 7',
     version: '50.0',
-    recordVideo: false,
   }, {
     browserName: 'firefox',
     platform: 'Windows 7',
     version: '46.0',
-    recordVideo: false,
   }, {
     browserName: 'firefox',
     platform: 'Windows 7',
     version: '47.0',
-    recordVideo: false,
   }, {
     browserName: 'firefox',
     platform: 'Windows 7',
     version: '48.0',
-    recordVideo: false,
   }, {
     browserName: 'MicrosoftEdge',
     platform: 'Windows 10',
     version: '13.10586',
-    recordVideo: false,
   }, {
     browserName: 'firefox',
     platform: 'Windows 10',
     version: '46.0',
-    recordVideo: false,
   }, {
     browserName: 'internet explorer',
     platform: 'Windows 10',
     version: '11.103',
-    recordVideo: false,
   }, {
     browserName: 'internet explorer',
     platform: 'Windows 8.1',
     version: '11.0',
-    recordVideo: false,
   }, {
     browserName: 'safari',
     platform: 'OS X 10.9',
     version: '7.0',
-    recordVideo: false,
   }, {
     browserName: 'safari',
     platform: 'OS X 10.10',
     version: '8.0',
-    recordVideo: false,
   }, {
     browserName: 'safari',
     platform: 'OS X 10.11',
     version: '9.0',
-    recordVideo: false,
   }, {
     browserName: 'Safari',
     appiumVersion: '1.5.3',
-    recordVideo: false,
     deviceName: 'iPhone 6s Simulator',
     deviceOrientation: 'portrait',
     platformVersion: '9.3',
@@ -84,7 +71,6 @@ module.exports = {
   }, {
     browserName: 'Safari',
     appiumVersion: '1.5.3',
-    recordVideo: false,
     deviceName: 'iPhone 6 Simulator',
     deviceOrientation: 'portrait',
     platformVersion: '9.2',
@@ -92,7 +78,6 @@ module.exports = {
   }, {
     browserName: 'Safari',
     appiumVersion: '1.5.3',
-    recordVideo: false,
     deviceName: 'iPhone 5s Simulator',
     deviceOrientation: 'portrait',
     platformVersion: '9.1',
